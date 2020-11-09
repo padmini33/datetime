80ef90c6c920b6b464a6f7d58e895e6896b223ee
Git Server Admin <admin@git-server>
2020-11-09 06:03:37 +0000

    Add initial repository

---
+++ .score.sh
@@ -0,0 +1,19 @@
+if [ -a .hidden.txt ]
+  then sudo rm .hidden.txt
+  fi
+#R CMD BATCH prog.R > .hidden.txt
+Rscript prog.R > .hidden.txt
+FS_SCORE=0 
+if [ $(grep -w "2016-12-25" .hidden.txt| wc -l) -gt 0 ] && [ $(grep -w "[1]" .hidden.txt| wc -l) -gt 0 ]
+  then FS_SCORE=$((FS_SCORE+ 100))
+fi
+echo "FS_SCORE:$FS_SCORE%"
+
+
+
+
+
+
+
+
+
